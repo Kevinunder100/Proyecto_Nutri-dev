e197b5943abb8002c35845df12f6db63bef3f928
Kevinunder100 <Kevinunder10@gmail.com>
2025-12-04 23:46:36 -0600

    Fix: Remove all project files from main, keep docs only

--- src/routes/ingredient.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { IngredientController } from "../controllers/ingredient.controller";
-
-const router = Router();
-const ingredientController = new IngredientController();
-
-router.get("/", ingredientController.getTopIngredients);
-
-export default router;
